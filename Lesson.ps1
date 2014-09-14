86f3ad7ed57240f4eeed753e5fd61b6372e83170
shashankp <shashank.pv@gmail.com>
2014-09-14 00:52:43 -1000

    init

---
+++ Lesson.ps1
@@ -0,0 +1,72 @@
+﻿##Lesson
+
+function Get-Lesson($LessonId)
+{
+    #TODO: return Array of Dictionary
+    return ,@(
+     )
+}
+
+function Start-Lesson($LessonId)
+{
+    #$Items = Get-Lesson("test")
+    $Items = @(
+        @{M="Welcome to your first lesson in PowerShell"; Invoke=$false},
+        @{Q="Powershell prints values as they are entered. Try entering number 9"; 
+          A="9";
+          #TODO: Message after correct answer
+          Invoke = $false},
+        @{Q="Adding numbers get evaluated as expected. Add numbers 3 and 5"; 
+          Clue="Type 3 + 5";
+          A="3+5"},
+        @{M="Congrats on completing your first lesson"; Invoke=$false}
+     )
+
+    $CompletedItems = 0
+    foreach($Item in $Items)
+    {
+        $FractionCompleted = $CompletedItems/$Items.Count
+        Display-Percentage-Completed-Message ($FractionCompleted)
+
+        $Completed = $false
+        $UserInput = ""
+        Do {
+            
+            if ($Item.ContainsKey("M")) {
+                ##Message
+                
+                Display-Message -msg $Item.'M' -pause $true
+                $Completed = $true
+            } else {
+                
+                ##Question
+                Display-Message -msg $Item.'Q'
+
+                Do {
+                    Write-Host "PS> " -NoNewline -ForegroundColor White
+                    $UserInput = Read-Host
+                    } while ($UserInput.Trim().Length -eq 0)
+
+                if (-not $Item.ContainsKey("Invoke") -or $Item.'Invoke' -eq $true) 
+                {  
+                    #$ExecutionContext.InvokeCommand.ExpandString($c)
+                    Invoke-Expression $UserInput 
+                }
+
+                #Answer
+                if ($UserInput -replace '\s+','' -eq $Item.'A')
+                {
+                    #Correct
+                    $Completed = $true        
+                    Display-Message "Correct"
+                } else {
+                    #Incorrect
+                    Display-Message "Oops. Try again."        
+                    #TODO: provide clue
+                }
+            }
+        } while ($Completed -eq $false)
+        $CompletedItems++
+    }
+    Display-Percentage-Completed-Message (1)
+}
---
+++ Spin.ps1
@@ -0,0 +1,74 @@
+﻿. .\Lesson.ps1
+
+function Pause
+{
+    #$Host.UI.RawUI.ReadKey() | Out-Null
+    Write-Host `n
+    $Ignore = Read-Host '::'
+}
+
+function Display-Message($Msg, $Pause)
+{
+    Write-Host " |  $Msg" -ForegroundColor Cyan
+    if ($Pause -eq $true) { Pause }
+}
+
+function Display-Percentage-Completed-Message($FractionCompleted)
+{
+    $PercentageCompleted = [System.Convert]::ToInt32(100 * $FractionCompleted)                
+    $TotalLength = ($host.UI.RawUI.BufferSize.Width - 20)
+    $StatusBar = "=" * [System.Convert]::ToInt32($FractionCompleted * $TotalLength)
+    $Spaces = " " * ($TotalLength-$StatusBar.Length)
+    $Msg = " |{0}{1}|  {2:d}% `n" -f $StatusBar, $Spaces, $PercentageCompleted
+    Write-Host $Msg -ForegroundColor Cyan
+}
+
+
+function Display-Menu($Message, $Options, $Targets)
+{
+    if ($Options.Count -ne $Targets.Count) 
+    {
+        Write-Error "Invalid args to Display-Menu"
+        return
+    }
+
+    Display-Message $Message    
+
+    #Display Menu
+    $i = 1
+    foreach($Option in $Options)
+    {
+        Write-Host "$i $Option"
+        $i++
+    }
+    #TODO: 0 to exit
+
+    #Read Selection
+    Write-Host `n
+    
+    #TODO: do-while
+    Write-Host "Selection: " -ForegroundColor Blue -NoNewline
+    $MenuSelection = Read-Host
+    if ($MenuSelection -ge 1 -and 
+        $MenuSelection -le $Targets.Count)
+    {
+        &$Targets[$MenuSelection-1]
+    }
+}
+
+function Start-Spin
+{
+    ##Greet
+    clear-Host
+    $UserName = Read-Host 'What shall I call you? '
+    Display-Message -msg "Hi $UserName"
+
+    #Menu
+    Display-Menu -Options @("TestLesson","Err") -Targets @("Start-Lesson","option2") -Message "Main Menu"
+
+    #Lesson
+}
+
+Display-Message "Hi! Hit Enter when you are ready to begin."
+Read-Host
+Start-Spin
